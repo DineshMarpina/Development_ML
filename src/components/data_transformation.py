c749d3746276049a2b97d5b17b9a7dfd0571ace7
Dinesh Marpina <91659006+DineshMarpina@users.noreply.github.com>
2024-12-18 00:49:51 +0530

    Update data_transformation.py - import code changed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,6 +1,6 @@
 import sys
 from dataclasses import dataclass
-sys.path.insert(0,'C:\\Users\\marpi\\Downloads\\AINEW\\ML_Production_code\\src')
+#sys.path.insert(0,'C:\\Users\\marpi\\Downloads\\AINEW\\ML_Production_code\\src')
 
 import numpy as np 
 import pandas as pd
@@ -9,11 +9,15 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
-from exception import CustomException
-from logger import logging
+#from exception import CustomException
+#from logger import logging
+from src.exception import CustomException
+from src.logger import logging
+
 import os
 
-from utils import save_object
+#from utils import save_object
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
